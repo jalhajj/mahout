c7699dd85b9647db8254fb97c1eee45a16ffe937
florestan.demoor <florestan.demoor@gmail.com>
2017-10-02 10:19:12 +0200

    Added KFold evaluator

---
+++ mr/src/main/java/org/apache/mahout/cf/taste/impl/eval/RMSRecommenderEvaluatorKFold.java
@@ -0,0 +1,33 @@
+package org.apache.mahout.cf.taste.impl.eval;
+
+import org.apache.mahout.cf.taste.impl.common.FullRunningAverage;
+import org.apache.mahout.cf.taste.impl.common.RunningAverage;
+import org.apache.mahout.cf.taste.model.Preference;
+
+public final class RMSRecommenderEvaluatorKFold extends AbstractKFoldRecommenderEvaluator {
+
+  private RunningAverage average;
+
+
+  @Override
+  protected void reset() {
+      average = new FullRunningAverage();
+  }
+
+  @Override
+  protected void processOneEstimate(float estimatedPreference, Preference realPref) {
+      double diff = realPref.getValue() - estimatedPreference;
+      average.addDatum(diff * diff);
+  }
+
+  @Override
+  protected double computeFinalEvaluation() {
+      return Math.sqrt(average.getAverage());
+  }
+
+  @Override
+  public String toString() {
+      return "RMSRecommenderEvaluator";
+  }
+  
+}
