cefe69a14306ebe4f9e73ee3fe472eea9a794dba
florestan.demoor <florestan.demoor@gmail.com>
2017-10-18 17:37:58 +0200

    Generating random parameters with PRNG for hash functions

--- mr/src/main/java/org/apache/mahout/cf/taste/impl/common/HashFunction.java
+++ mr/src/main/java/org/apache/mahout/cf/taste/impl/common/HashFunction.java
@@ -1,6 +1,9 @@
 package org.apache.mahout.cf.taste.impl.common;
 
 import java.math.BigInteger;
+import java.util.Random;
+
+import gnu.trove.list.array.TLongArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,35 +13,63 @@ class HashFunction {
   private static final Logger log = LoggerFactory.getLogger(HashFunction.class);
   
   private BigInteger bigPrime = new BigInteger("9223372036854775783");
+    
+  /* Store some random parameters that should be the same for everyone
+   * Parameters are generated in a lazy way when asked for
+   */
+  private static int index = -1;
+  private static TLongArrayList randomParamA = new TLongArrayList();
+  private static TLongArrayList randomParamB = new TLongArrayList();
   
-  private static String[] randomParamA = {
-    "3855483969251045550", "2274948550203382259", "8086346051022946365", "3742599363140121417",
-    "8127506216481886778", "8992508990183509581", "6824159666599056129", "1515839073414842035",
-    "3528950968555460028", "3742666563140121417", "8086685051022946685", "8926282854497893686",
-    "7549048658687782964", "2254963801332704518", "4662970043116588076", "1164415423486499490",
-    "8596154617854201450", "35867659355547039",   "7491907009982470321", "353950969553360028",
-    "3742666563140908304"};
-                                
-  private static String[] randomParamB = {
-    "8708688407112891618", "7360455785926943020", "8825528156902469898", "7810309941732699355",
-    "2097246509310169464", "238488925168692289" , "260263731934835712",  "688270280519291225",
-    "2179209161542291414", "7816665941732699355", "6855528156906859898", "4390665439843151325",
-    "8145312472828294149", "2847806146481835937", "3995608470880119485", "947734926901625025",
-    "4850723540680713533", "35867659355547039",   "7491907009982470321", "2079208161542291414",
-    "9083045941732699355"};
-   
+  /* Initliaze a PRNG with the time seed
+   * Another seed can be given using `initSeet(long s)` method
+   */
+  private static long seed = System.currentTimeMillis();
+  private static Random rand = new Random(seed);
+  
+  /* Store parameters for hash computations */
   private final BigInteger a;
   private final BigInteger b;
   private final BigInteger w;
   
+  
+  /** Initialize the PRNG with a given seed
+   * 
+   *  @param  s   seed
+   */
+  public static void initSeed(long s) {
+    seed = s;
+    rand = new Random(seed);
+  }
+  
+  
+  /** Return a hash function for a given iteration index and width
+   * 
+   *  @param  iteration   Used to choose the parameters of the hash function
+   *  @param  width       Range: a key will be hashed into {0, .., width - 1}
+   */
   HashFunction(int iteration, int size) {
     
-    a = new BigInteger(randomParamA[iteration]);
-    b = new BigInteger(randomParamB[iteration]);
+    /* Check if enough random parameters already generated, otherwise generate some more */
+    if (iteration >= index) {
+      for (int i = index + 1; i <= iteration; i++) {
+        long a = rand.nextLong();
+        long b = rand.nextLong();
+        randomParamA.set(i, a);
+        randomParamB.set(i, b);
+      }
+      index = iteration;
+    }
+    
+    /* Store the parameters chosen */
+    a = BigInteger.valueOf(randomParamA.get(iteration));
+    b = BigInteger.valueOf(randomParamB.get(iteration));
     w = BigInteger.valueOf(size);
     
   }
   
+  
+  /** Hashes a key and returns an integer */
   int hash(long key) {
     BigInteger k = BigInteger.valueOf(key);
     return a.multiply(k).add(b).mod(bigPrime).mod(w).intValue();
