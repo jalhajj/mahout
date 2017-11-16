d3dbd6ab8cc48826f3423f539d406c6ee1f61e25
Florestan De Moor <florestan.demoor@gmail.com>
2017-11-16 16:30:33 +0100

    New optimization problem for CM config

--- mr/src/main/java/org/apache/mahout/cf/taste/impl/common/CountMinSketchConfig.java
+++ mr/src/main/java/org/apache/mahout/cf/taste/impl/common/CountMinSketchConfig.java
@@ -19,18 +19,14 @@ import org.apache.mahout.cf.taste.model.PreferenceArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class CountMinSketchConfig implements Serializable {
   
   private static final transient Logger log = LoggerFactory.getLogger(CountMinSketchConfig.class);
   
-  private transient int MAX_WIDTH = 10000;
   private transient int MAX_DEPTH = 25;
+  private transient int MIN_DEPTH = 1;
   
-  private final double gamma; // Deniability wished
-  private final double betaC; // Error bound wished for cosine query
-  private final double betaP; // Error bound wished for point query
+  private final double q; // Privacy/Accuracy trade-off parameter
   private EDResult result;
   
   
@@ -52,29 +48,25 @@ public class CountMinSketchConfig implements Serializable {
    *  @param    g   gamma-deniability condition value
    *  @param    e   Error bound value
    */
-  public CountMinSketchConfig(double g, double bc, double bp) {
-    gamma = g;
-    betaC = bc;
-    betaP = bp;
+  public CountMinSketchConfig(double q_) {
+    q = q_;
     result = null;
   }
   
   
   /** Configure the count-min sketch delta and epsilon parameters
-   *  to ensure a given level of deniability and to ensure a
-   *  probabilistic error bound on the accuracy
    * 
    *  Must be called before getDelta() and getEpsilon()
    * 
    *  @param  dataModel     Dataset
-   *  @param  datasetPath   Path of the dataset file, used as dataset
+   *  @param  datasetName   Name of the dataset file, used as dataset
    *                        identifier for serialization
    * 
    *  @throws TasteException 
    */
-  public void configure(DataModel dataModel, String datasetPath) throws TasteException {
-    String datasetName = StringUtils.substringBefore(datasetPath.replace("/", "-"), ".");
-    String path = "ser/" + datasetName + "_gamma_" + gamma + "_betaC_" + betaC + "_betaP_" + betaP + ".ser";
+  public void configure(DataModel dataModel, String datasetName) throws TasteException {
+
+    String path = "ser/" + datasetName + "_q_" + q + ".ser";
     log.info("Try to find {} file, check if already computed", path);
     try {
       /* Check if computation was already serialized in a previous run */
@@ -115,7 +107,7 @@ public class CountMinSketchConfig implements Serializable {
   }
     
   
-  /** Compute epsilon and delta to meet the conditions required
+  /** Compute epsilon and delta by solving optimization problem
    *  
    *  @throws TasteException    If not possible to meet the conditions
    */ 
@@ -123,7 +115,7 @@ public class CountMinSketchConfig implements Serializable {
     
     LongPrimitiveIterator it = dataModel.getUserIDs();
     int n = 0;
-    int I = dataModel.getNumItems();
+    int u = dataModel.getNumItems();
     
     /* NOTE: For now, consider all users, but in the future, we may want to not
      * bother about those with huge profiles. For instance, the width chosen
@@ -136,204 +128,92 @@ public class CountMinSketchConfig implements Serializable {
       n = Math.max(n, profileSize);
     }
     
-    /* Try to find a solution with branch and bound / binary search techniques */
-    int minWidth = getWidthForCosineError(n);
-    Integer minDepth = 1;
-    int w;
-    for (w = minWidth; w < MAX_WIDTH; w++) {
-      minDepth = getDepthForPointError(w, n);
-      if (minDepth == null) {
-        continue;
-      } else {
-        minDepth = getDepthForDeniability(I, n, w, minDepth);
-        if (minDepth == null) {
-          continue;
-        } else {
-          break;
-        }
-      }
-    }
+    int bestWidth = 0;
+    int bestDepth = 0;
+    double bestMax = 0;
+    for (int d = MIN_DEPTH; d < MAX_DEPTH; d++) {
+			for (int w = d; w <= n; w++) {
+				double x = Fmeasure(w, d, n, u, q);
+				if (x >= bestMax) {
+					bestWidth = w;
+					bestDepth = d;
+					bestMax = x;
+				}
+			}
+		}
     
     /* Check if a solution was found */
-    if (w == MAX_WIDTH) {
-      throw new TasteException("Not possible to meet conditions, try with different parameters");
+    if (bestWidth == 0 && bestDepth == 0) {
+      throw new TasteException("No solution found (this should not happen)");
     }
     
     /* Compute the chosen parameters */
-    double epsilon = Math.exp(1) / (double) minWidth;
-    double delta = Math.exp(- (double) minDepth);
+    double epsilon = Math.exp(1) / (double) bestWidth;
+    double delta = Math.exp(- (double) bestDepth);
     result = new EDResult(delta, epsilon);
     log.info("Parameters chosen: width={} (epsilon={}), depth={} (delta={})",
-              minWidth, epsilon, minDepth, delta);
+              bestWidth, epsilon, bestDepth, delta);
   }
   
   
-  /** Compute gamma-deniability with the approximation formula
+  /** Compute the inserted probability
    * 
-   * @param   u   Total number of keys
-   * @param   n   Number of keys inserted in the sketch
    * @param   w   Width of the sketch
    * @param   d   Depth of the sketch
+   * @param   n   Number of keys inserted in the sketch
+   * @param   u   Total number of keys
    * 
-   * @return  gamma-deniability value
+   * @return  inserted probability value
    */
-  private double gammaDeniability(int u, int n, int w, int d) {
-    double U = (double) u;
-    double N = (double) n;
-    double W = (double) w;
+  public static double probaInserted(int w, int d, int n, int u) {
+		double W = (double) w;
     double D = (double) d;
+    double N = (double) n;
+    double U = (double) u;
     
-    double cardDiff = U - N;
-    double invWidth = 1 / W;
-    double p = 1 - Math.pow(1 - invWidth, N);
-    double q = 1 - Math.pow(1 - invWidth / p, cardDiff * p);
-    return Math.pow(q, D);
+    double falseP = Math.pow(1 - Math.pow(1 - 1 / W, N), D);
+    return N / (N + falseP * (U - N));
   }
   
   
-  /** Get the maximum depth to ensure a given deniability for a given user
+  /** Compute the not-exact retrieve probability
    * 
-   *  @param  I         Total number of items
-   *  @param  n         Maximum number of keys to insert in a sketch
-   *  @param  width     Width of the count-min sketch
-   *  @param  minDepth  Minimum value required for the depth
-   * 
-   *  @return   Minimum depth >= minDepth that ensures the deniability condition is met
-   *            or null if not possible
-   * 
-   *  @throws TasteException    If not possible to meet the condition
+   * @param   w   Width of the sketch
+   * @param   d   Depth of the sketch
+   * @param   n   Number of keys inserted in the sketch
+   *
+   * @return	the probability for a point-query 
+   * 					to NOT retrieve the exact value
    */
-  private Integer getDepthForDeniability(int I, int n, int width, int minDepth) throws TasteException {
+  public static double probaNotExactRetrieve(int w, int d, int n) {
+		double W = (double) w;
+    double D = (double) d;
+    double N = (double) n;
     
-    double currentDen;
-    int currentDepth;
-    int da = minDepth, db = MAX_DEPTH;
-    
-    /* Check if min depth is small enough to find a solution, not possible otherwise */
-    currentDen = gammaDeniability(I, n, width, da);
-    if (currentDen < gamma) {
-      return null;
-    }
-    
-    /* Start binary search */
-    while (da < (db - 1)) {
-      log.debug("For deniability, search between {} and {}", da, db);
-      currentDepth = (da + db) / 2;
-      currentDen = gammaDeniability(I, n, width, da);
-      if (currentDen < gamma) {
-        db = currentDepth;
-      } else {
-        da = currentDepth;
-      }
-    }
-    log.debug("For gamma={} and width={}, depth selected to meet deniability condition is {}", gamma, width, da);
-    return new Integer(da);
-  }
-  
-  
-  /** Compute the expected value for the percentage of collisions in a row
+		return Math.pow(1 - Math.pow(1 - 1 / W, N), D);
+	}
+	
+	
+	/** Function the optimization problem seeks to maximize
+	 *  Inspired from precision / recall f-measure
+	 * 
+   * @param   w   Width of the sketch
+   * @param   d   Depth of the sketch
+   * @param   n   Number of keys inserted in the sketch
+   * @param   u   Total number of keys
+   * @param		q		Accuracy / privacy trade-off parameter
    * 
-   *  @param  width   width of the count-min sketch
-   *  @param  nb      number of elements inserted
-   * 
-   *  @return   expected percentage of collisions in a row
+   * @return	f-measure value
    */
-  private double expectedPercentageColisions(int width, int nb) {
-    double w = (double) width;
-    double n = (double) nb;
-    return (n - w * (1 - Math.pow(1 - 1 / w, n))) / n;
-  }
-  
-  
-  /** Get the width to ensure a given error bound is met for a given user
-   * 
-   *  @param    n   Maximum number of keys to insert in a sketch
-   * 
-   *  @return   Minimum width to ensure the error condition for cosine query is met
-   * 
-   *  @throws TasteException    If not possible to meet the condition
-   */
-  private int getWidthForCosineError(int n) throws TasteException {
-    
-    double currentError;
-    int currentWidth;
-    int wa = 1, wb = MAX_WIDTH;
-    
-    /* Check if max width is big enough to find a solution, not possible otherwise */
-    currentError = expectedPercentageColisions(wb, n);
-    if (currentError > betaC) {
-      String msg = String.format("Not possible to meet cosine error <= %g condition, would require a width greater than %d",
-                                betaC, wb);
-      throw new TasteException(msg);
-    }
-    
-    /* Start binary search */
-    while (wa < (wb - 1)) {
-      log.debug("For cosine error, search between {} and {}", wa, wb);
-      currentWidth = (wa + wb) / 2;
-      currentError = expectedPercentageColisions(currentWidth, n);
-      if (currentError > betaC) {
-        wa = currentWidth;
-      } else {
-        wb = currentWidth;
-      }
-    }
-    log.debug("For cosine error={}, width selected to meet condition is {}", betaC, wb);
-    return wb;
-  }
-  
-  
-  /** Compute the probability that a point query does not return the exact value
-   * 
-   *  @param  width   Width of the count-min sketch
-   *  @param  depth   Depth of the count-min sketch
-   *  @param  nb      Number of elements inserted
-   * 
-   *  @return   expected percentage of collisions in a row
-   */
-  private double notExactPointQueryProba(int width, int depth, int nb) {
-    if (nb == 0) { return 0.0; }
-    double w = (double) width;
-    double d = (double) depth;
-    double n = (double) nb;
-    return Math.pow(1 - Math.pow(1 - 1 / w, nb - 1), d);
-  }
-  
-  
-  /** Get the depth to ensure a given error bound is met for a given user
-   * 
-   *  @param    n   Maximum number of keys to insert in a sketch
-   * 
-   *  @return   Minimum depth to ensure the error condition for point query is met
-   *            or null if not possible
-   * 
-   *  @throws TasteException    If not possible to meet the condition
-   */
-  private Integer getDepthForPointError(int width, int n) throws TasteException {
-    
-    double currentError;
-    int currentDepth;
-    int da = 1, db = MAX_DEPTH;
-    
-    /* Check if max depth is big enough to find a solution, not possible otherwise */
-    currentError = notExactPointQueryProba(width, db, n);
-    if (currentError > betaP) {
-      return null;
-    }
-    
-    /* Start binary search */
-    while (da < (db - 1)) {
-      log.debug("For point error, search between {} and {}", da, db);
-      currentDepth = (da + db) / 2;
-      currentError = notExactPointQueryProba(width, currentDepth, n);
-      if (currentError > betaP) {
-        da = currentDepth;
-      } else {
-        db = currentDepth;
-      }
-    }
-    log.debug("For point error={}, width selected to meet condition is {}", betaP, db);
-    return new Integer(db);
+	public static double Fmeasure(int w, int d, int n, int u, double q) {
+    double beta = 1 - probaNotExactRetrieve(w, d, n);
+    double p = 1 - probaInserted(w, d, n, u);
+    if (beta == 0 || p == 0) {
+			return 0;
+		} else {
+			double q2 = Math.pow(q, 2);
+			return (1 + 2) * beta * p / (q2 * beta + p);
+		}
   }
   
   
@@ -363,7 +243,6 @@ public class CountMinSketchConfig implements Serializable {
     } else
     return result.epsilon;
   }
-  
   
   
 }
